ed51dd4af051d1b69c2ccda29d0e36dd6ef3859c
David J. Hwang <jeongjaehwang@gmail.com>
2023-12-20 17:07:56 +0900

    add files from Section 41

---
+++ app.js
@@ -0,0 +1,95 @@
+// ======================== MongoDB server command ========================
+// - Open mongod: brew services start mongodb-community
+// - Open mongosh: mongosh
+// ========================================================================
+
+// ======================== External Modules ========================
+const colors = require('colors')
+// ==================================================================
+
+// ======================== Express Server ========================
+const express = require('express')
+const app = express()
+const PORT = 3000
+// ================================================================
+
+// ======================== Using mongoose ========================
+const mongoose = require('mongoose')
+const Campground = require('./models/campground')
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+const db = mongoose.connection
+db.on('error', console.error.bind(console, "connection error:"))
+db.once('open', () => console.log('DATABASE CONNECTED!'.bgCyan))
+// ================================================================
+
+// ======================== Using ejs ========================
+const path = require('path')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+// ===========================================================
+
+// ======================== Using ejs-mate ========================
+const ejsMate = require('ejs-mate')
+app.engine('ejs', ejsMate)
+// ================================================================
+
+// ======================== Using method-override ========================
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+// =======================================================================
+
+// ======================== Using body-parser ========================
+app.use(express.urlencoded({ extended: true }))
+// ===================================================================
+
+// ======================== Routes ========================
+app.get('/', (req, res) => {
+  res.render('campgrounds/home')
+})
+
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({})
+  res.render('campgrounds/index', { campgrounds })
+})
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req, res) => {
+  // res.send(req.body)
+  const campground = new Campground(req.body.campground)
+  await campground.save()
+  res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params
+  const campground = await Campground.findById(id)
+  res.render('campgrounds/show', { campground })
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const { id } = req.params
+  const campground = await Campground.findById(id)
+  res.render('campgrounds/edit', { campground })
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+  // res.send("IT WORKED!")
+  const { id } = req.params
+  const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}, { new: true })
+  res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params
+  await Campground.findByIdAndDelete(id)
+  res.redirect('/campgrounds')
+})
+
+// ========================================================
+
+// ======================== RUN SERVER ========================
+app.listen(PORT, () => console.log(`SERVER LISTENING ON PORT: ${PORT}...`.bgCyan))
+// ============================================================
---
+++ seeds/index.js
@@ -0,0 +1,44 @@
+const colors = require('colors')
+
+const cities = require('./cities')
+const { places, descriptors } = require('./seedHelpers')
+const mongoose = require('mongoose')
+const Campground = require('../models/campground')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+const db = mongoose.connection
+db.on('error', console.error.bind(console, "connection error:"))
+db.once('open', () => console.log('DATABASE CONNECTED!'.bgCyan))
+
+
+const sample = array => array[Math.floor(Math.random() * array.length)]
+
+// console.log(places[0])
+// console.log(descriptors[0])
+
+// console.log(sample(places))
+// console.log(sample(descriptors))
+
+
+const seedDB = async () => {
+  // await Campground.deleteMany({})
+  // const c = new Campground({title: 'purple field'})
+  // await c.save()
+  await Campground.deleteMany({})
+  for(let i = 0; i < 50; i++){
+    const random1000 = Math.floor(Math.random() * 1000)
+    const price = Math.floor(Math.random() * 20) + 10
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://source.unsplash.com/collection/483251`,
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas, excepturi magni nemo incidunt et ducimus optio corrupti consectetur expedita exercitationem laboriosam numquam nulla minus ex magnam! Ipsum sint quasi molestias?",
+      price,
+    })
+    await camp.save()
+  }
+}
+
+seedDB().then(() => {
+  mongoose.connection.close()
+})
