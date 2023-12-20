f92d494d10a88c4471f17b18d0561716a232155d
David J. Hwang <jeongjaehwang@gmail.com>
2023-12-20 19:34:03 +0900

    add custom basic error handler

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ const app = express()
 const PORT = 3000
 // ================================================================
 
+// ======================== Using Async Error Handler ========================
+const catchAsync = require('./utils/catchAsync')
+// ===========================================================================
+
 // ======================== Using mongoose ========================
 const mongoose = require('mongoose')
 const Campground = require('./models/campground')
@@ -50,48 +54,58 @@ app.get('/', (req, res) => {
   res.render('campgrounds/home')
 })
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
   const campgrounds = await Campground.find({})
   res.render('campgrounds/index', { campgrounds })
-})
+}))
 
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', catchAsync(async (req, res, next) => {
   // res.send(req.body)
   const campground = new Campground(req.body.campground)
   await campground.save()
   res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params
   const campground = await Campground.findById(id)
   res.render('campgrounds/show', { campground })
-})
+}))
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
   const { id } = req.params
   const campground = await Campground.findById(id)
   res.render('campgrounds/edit', { campground })
-})
+}))
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
   // res.send("IT WORKED!")
   const { id } = req.params
   const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}, { new: true })
   res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync( async (req, res) => {
   const { id } = req.params
   await Campground.findByIdAndDelete(id)
   res.redirect('/campgrounds')
+}))
+// ========================================================
+
+// ======================== ERROR HANDLER ========================
+app.all('*', (req, res, next) => {
+  res.send("404 NOT FOUND")
 })
 
-// ========================================================
+
+app.use((err, req, res, next) => {
+  res.send('Oh Boy, something went wrong!')
+})
+// ===============================================================
 
 // ======================== RUN SERVER ========================
 app.listen(PORT, () => console.log(`SERVER LISTENING ON PORT: ${PORT}...`.bgCyan))
