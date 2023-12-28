4545a00dcd43b108df8f52af8e287a779568a1f7
David J. Hwang <jeongjaehwang@gmail.com>
2023-12-29 00:57:42 +0900

    add controllers and stars

--- routes/users.js
+++ routes/users.js
@@ -3,52 +3,36 @@ const router = express.Router()
 const User = require('../models/user')
 const passport = require('passport')
 
+const users = require('../controllers/users')
+
 const catchAsync = require('../utils/catchAsync')
 const ExpressError = require('../utils/ExpressError')
 
 const { storeReturnTo } = require('../middleware')
 
-router.get('/register', (req, res) => {
-  res.render('users/register')
-})
 
-router.post('/register', catchAsync(async (req, res) => {
-  // res.send(req.body)
-  try {
-    const { email, username, password } = req.body;
-    const user = new User({ email, username })
-    const registeredUser = await User.register(user, password)
-    // console.log(registeredUser)
-    req.login(registeredUser, err => {
-      if(err) return next(err);
-      req.flash('success', 'Welcome to YelpCamp!')
-      res.redirect('/campgrounds')
-    })
-  } catch (e) {
-    req.flash('error', e.message)
-    res.redirect('/register')
-  }
-}))
 
-router.get('/login', (req, res) => {
-  res.render('users/login')
-})
+// ============== ROUTES ==============
+router.route('/register')
+.get(users.renderRegister)
+.post(catchAsync(users.register))
 
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-  req.flash('success', 'Welcome back!')
-  const redirectUrl = res.locals.returnTo || '/campgrounds'
-  delete req.session.returnTo
-  res.redirect(redirectUrl)
-})
+router.route('/login')
+.get(users.renderLogin)
+.post(storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
-router.get('/logout', (req, res, next) => {
-  req.logout(function(err) {
-    if(err) {
-      return next(err)
-    }
-    req.flash('success', "Goodbye!")
-    res.redirect('/campgrounds')
-  })
-})
+router.get('/logout', users.logout)
+
+// ====================================
+
+// router.get('/register', users.renderRegister)
+
+// router.post('/register', catchAsync(users.register))
+
+// router.get('/login', users.renderLogin)
+
+// router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
+
+// router.get('/logout', users.logout)
 
 module.exports = router;
