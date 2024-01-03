52e2d06d0a399c239869eb84d7f859a878c4d6ad
David J. Hwang <jeongjaehwang@gmail.com>
2024-01-04 00:40:33 +0900

    add more styles

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -8,6 +8,8 @@ center: campground.geometry.coordinates,
 zoom: 9,
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 new mapboxgl.Marker()
   .setLngLat(campground.geometry.coordinates)
   .setPopup(
